2b73b62abb240ca114245e4379166bcd6de8fc86
Fil Maj <filip.maj@nitobi.com>
2010-10-31 02:12:05 -0700

    Added in helper qunit async "chain" function. Moar tests (scrolling and config tests pre-impl).

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,14 @@
+// A little sugar on top of qunit
+QUnit.chain = function(tests, delay) {
+    QUnit.stop();
+    if (tests.length > 0) {
+        setTimeout(function() {
+            tests.pop()();
+            QUnit.chain(tests, delay);
+        }, delay);
+    } else QUnit.start();
+};
+
 function Tests() { 
     this.bounceTimeout = 3000;
     this.scrollTimeout = 1000;
@@ -13,7 +24,7 @@ Tests.prototype = {
     },
     Test:function() {
         var self = this;
-        module('Initialization', {
+        module('Construction/destruction', {
            setup:function() {
   //             glovebox = new GloveBox("masterList");
            },
@@ -21,8 +32,8 @@ Tests.prototype = {
 //               glovebox = null;
            }
         });
-        test('We can instantiate glovebox', function() {
-            expect(4);
+        test('Baseline GloveBox initilization', function() {
+            expect(6);
             var glovebox1 = new GloveBox("masterList");
             ok(typeof glovebox1 == 'object', 'GloveBox can be instantiated via ID string');
             var glovebox2 = new GloveBox(document.getElementById("masterList"));
@@ -33,6 +44,18 @@ Tests.prototype = {
                 ok(true, 'Expected exception raised when wrong ID string provided on construction' + e.type);
                 equals(e.message, "Cannot read property 'parentNode' of null");
             }
+            ok(glovebox2.scrollY, 'Default scrolling direction is vertical');
+            ok(!glovebox2.scrollX, 'Horizontal scrolling is off by default');
+        });
+        test('GloveBox destruction', function(){
+            // does it clean up after itself? any side effects from defining two gloveboxes on the same element?
+        })
+        test('GloveBox options configuration', function() {
+            expect(3);
+            var glovebox = new GloveBox('masterList', { scrollX:true, scrollY:true, parentHeight:'100px'});
+            ok(glovebox.scrollX, 'scrollX=true in options object should be true in GloveBox object');
+            ok(glovebox.scrollY, 'scrollY=true in options object should be true in GloveBox object');
+            equals(document.getElementById('masterList').parentNode.style.height, '100px', 'Can set GloveBox element\'s parent\'s height')
         });
         
         module('Scrolling', {
@@ -43,30 +66,35 @@ Tests.prototype = {
                 glovebox = null;
             }
         });
-        test('Vertical scrolling test in range', function() {
-            QUnit.stop();
+        test('Scrolling directions', function() {
             expect(1);
-            glovebox.y = -100;
-            glovebox.finishScrolling();
-            setTimeout(function() {
-                equals(glovebox.y, -100, 'Y setter works');
-                start();
-            }, self.scrollTimeout);
+            QUnit.chain([function() {
+                glovebox.scrollY = false;
+                glovebox.scrollX = true;
+                glovebox.y = -100;
+                glovebox.finishScrolling();
+                equals(glovebox.y, 0, 'Setting y when scrollY = false should do nothing');
+            }], self.scrollTimeout);
+        });
+        test('Vertical scrolling test in range', function() {
+            expect(1);
+            QUnit.chain([function() {
+                glovebox.y = -100;
+                glovebox.finishScrolling();
+                equals(glovebox.y, -100, 'Setting y to in-range value works');
+            }], self.scrollTimeout);
         });
         test('Vertical scrolling test out of range', function() {
-            QUnit.stop();
             expect(2);
-            glovebox.y = 100;
-            glovebox.finishScrolling();
-            setTimeout(function() {
-                equals(glovebox.y, 0, 'Y setter works');                
+            QUnit.chain([function() {
+                glovebox.y = 100;
+                glovebox.finishScrolling();
+                equals(glovebox.y, 0, 'Setting y to out-of-range positive value works');
+            }, function() {
                 glovebox.y = -10000;
                 glovebox.finishScrolling();
-                setTimeout(function() {
-                    equals(glovebox.y, -800); // refactor these properties.
-                    start();
-                }, self.bounceTimeout);
-            }, self.scrollTimeout);
+                equals(glovebox.y, -800, 'Setting y to out-of-range negative value works');
+            }], self.bounceTimeout);
         });
     }
 }
